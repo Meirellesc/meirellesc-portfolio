fc0f502fb0a7e702932f27f9a273a4636cdf40b2
Lucas Meirelles <lucas.carregaro@lg.com.br>
2024-05-27 14:39:06 -0300

    [FEAT] - Added the mobile layout and loading page. (missing details)

--- MeirellescPortfolio/Layout/MainLayout.razor.cs
+++ MeirellescPortfolio/Layout/MainLayout.razor.cs
@@ -2,16 +2,56 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.Localization;
 using MudBlazor;
+using MudBlazor.Services;
 using MudBlazor.Utilities;
+using System.Diagnostics;
 
 namespace MeirellescPortfolio.Layout
 {
-    public partial class MainLayout : LayoutComponentBase
+    public partial class MainLayout : LayoutComponentBase, IBrowserViewportObserver, IAsyncDisposable
     {
         [Inject] IStringLocalizer<Resource>? StringLocalizer { get; set; }
         [Inject] NavigationManager? NavigationManager { get; set; }
+        [Inject] IBrowserViewportService? BrowserViewportService { get; set; }
 
+        Guid IBrowserViewportObserver.Id { get; } = Guid.NewGuid();
+        private Breakpoint _breakpoint;
         private bool _isHomePage;
+
+        private bool _isOpen = false;
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await BrowserViewportService!.SubscribeAsync(this, fireImmediately: true);
+            }
+
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+        public async ValueTask DisposeAsync() => await BrowserViewportService!.UnsubscribeAsync(this);
+
+        public void ToggleOpen()
+        {
+            Debug.Print($"Menu Antes = { _isOpen}");
+            _isOpen = !_isOpen;
+            Debug.Print($"Menu Depois = {_isOpen}");
+        }
+
+        ResizeOptions IBrowserViewportObserver.ResizeOptions { get; } = new()
+        {
+            ReportRate = 250,
+            NotifyOnBreakpointOnly = true
+        };
+
+
+        Task IBrowserViewportObserver.NotifyBrowserViewportChangeAsync(BrowserViewportEventArgs browserViewportEventArgs)
+        {
+            _breakpoint = browserViewportEventArgs.Breakpoint;
+            return InvokeAsync(StateHasChanged);
+        }
 
         protected override void OnInitialized()
         {
