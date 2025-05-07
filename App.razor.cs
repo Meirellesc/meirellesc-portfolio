e051e3f6af7ade45f4e657fc37baaa23e3b7bd7f
Meirellesc <lucasmeirellesc@hotmail.com>
2025-05-07 09:12:07 -0300

    [HOTFIX] - Implemented the redirect function, including 404 HTML file, because the GitHub Pages is a static server and cannot solve clients routes.

---
+++ App.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace MeirellescPortfolio
+{
+    public partial class App : ComponentBase
+    {
+        [Inject] NavigationManager? Navigation { get; set; };
+
+        protected override void OnInitialized()
+        {
+            var uri = new Uri(Navigation.Uri);
+            var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+            var redirect = query["redirect"];
+
+            if (!string.IsNullOrEmpty(redirect))
+            {
+                Navigation.NavigateTo(redirect, true);
+            }
+        }
+    }
+}
