ad8ec53cf8abf36380fa65e455fb5d74e2564e4d
Meirellesc <lucasmeirellesc@hotmail.com>
2025-05-07 09:23:29 -0300

    [HOTFIX] - Fixed the infinite loading redirect.

--- App.razor.cs
+++ App.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.IO;
+using Microsoft.AspNetCore.Components;
 
 namespace MeirellescPortfolio
 {
@@ -9,10 +10,12 @@ namespace MeirellescPortfolio
         protected override void OnInitialized()
         {
             var uri = new Uri(Navigation.Uri);
+            var path = uri.AbsolutePath.ToLowerInvariant();
             var query = System.Web.HttpUtility.ParseQueryString(uri.Query);
             var redirect = query["redirect"];
 
-            if (!string.IsNullOrEmpty(redirect))
+            // Redireciona apenas se estiver em /index.html com redirect
+            if (path.EndsWith("/index.html") && !string.IsNullOrEmpty(redirect))
             {
                 Navigation.NavigateTo(redirect, true);
             }
