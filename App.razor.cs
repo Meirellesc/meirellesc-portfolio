16d3e181ee43b232579433188bfe2e543c9b1258
Meirellesc <lucasmeirellesc@hotmail.com>
2025-07-15 03:36:14 -0300

    [HOTFIX] - removed the redirect function to avoid the infinity loading.

--- App.razor.cs
+++ App.razor.cs
@@ -5,30 +5,5 @@ namespace MeirellescPortfolio
 {
     public partial class App : ComponentBase
     {
-        [Inject] NavigationManager? Navigation { get; set; }
-
-        private static bool _hasRedirected = false;
-
-        protected override void OnInitialized()
-        {
-            if (_hasRedirected) return;
-
-            //Exemplo: uri= https://localhost:7200/projects
-            Uri? uri = new Uri(Navigation.Uri);
-
-            // Exemplo: projects
-            string? path = uri.AbsolutePath.ToLowerInvariant();
-
-            System.Collections.Specialized.NameValueCollection? query = System.Web.HttpUtility.ParseQueryString(uri.Query);
-
-            string? redirect = query["redirect"];
-
-            // Redireciona apenas se estiver em /index.html com redirect
-            if (Navigation.Uri.Contains("redirect=") && !path.Equals(redirect, StringComparison.OrdinalIgnoreCase))
-            {
-                _hasRedirected = true;
-                Navigation.NavigateTo(redirect, true);
-            }
-        }
     }
 }
