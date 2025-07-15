46b3bdc797a309b2803ee9e7a97fbcf5ed8c2d8b
Meirellesc <lucasmeirellesc@hotmail.com>
2025-07-15 03:56:22 -0300

    [HOTFIX] - Returned the redirect to the App razor.

--- App.razor.cs
+++ App.razor.cs
@@ -5,5 +5,30 @@ namespace MeirellescPortfolio
 {
     public partial class App : ComponentBase
     {
+        [Inject] NavigationManager? Navigation { get; set; }
+
+        private static bool _hasRedirected = false;
+
+        protected override void OnInitialized()
+        {
+            if (_hasRedirected || Navigation == null) return;
+
+            Uri? uri = new Uri(Navigation.Uri);
+
+            string? path = uri.AbsolutePath.ToLowerInvariant();
+
+            System.Collections.Specialized.NameValueCollection? query = System.Web.HttpUtility.ParseQueryString(uri.Query);
+
+            string? redirect = query["redirect"];
+
+            // Redireciona apenas se estiver em /index.html com redirect
+            if (!string.IsNullOrEmpty(redirect) && 
+                Navigation.Uri.Contains("redirect=") && 
+                !path.Equals(redirect, StringComparison.OrdinalIgnoreCase))
+            {
+                _hasRedirected = true;
+                Navigation.NavigateTo(redirect, true); // Ensure redirect is not null or empty
+            }
+        }
     }
 }
